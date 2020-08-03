ade6bb06fe8fb1e8c5df0dfdd7672c57549ce345
thangkp3 <thangkp3@gmail.com>
2020-08-03 21:29:26 +0700

    add todolist

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './App.scss';
 import ColorBox from './components/ColorBox';
 import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 
 function App() {
   const [todoList, setTodoList] = useState([
@@ -20,9 +21,21 @@ function App() {
     setTodoList(newTodoList);
   }
 
+  function handleTodoFormSubmit(formValues){
+    console.log('Form submit: ', formValues);
+    // add new todo to current todo list
+    const newTodo = {
+      id: todoList.length + 1,
+      ... formValues,
+    }
+    const newTodoList = [...todoList];
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList); 
+  }
+
   return (
     <div className="app">
-
+      <TodoForm onSubmit = {handleTodoFormSubmit}/>
       <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
