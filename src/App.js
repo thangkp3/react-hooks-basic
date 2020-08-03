ccf4e748d627f4769695482575c15fe740797404
thangkp3 <thangkp3@gmail.com>
2020-08-03 09:29:03 +0700

    changed color

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import ColorBox from './components/ColorBox';
 
 function App() {
   return (
     <div className="app">
-
+      <ColorBox />
     </div>
   );
 }
---
+++ src/components/ColorBox/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import './ColorBox.scss';
+
+ColorBox.propTypes = {
+
+};
+
+function getRandomColor() {
+    const COLOR_LIST = ['deeppink', 'green', 'yellow', 'black', 'blue']
+    const randomIndex = Math.trunc(Math.random() * 5);
+    return COLOR_LIST[randomIndex]
+}
+
+function ColorBox() {
+
+    const [color, setColor] = useState(() => {
+        const initColor = localStorage.getItem('box_color') || 'green'
+        return initColor;
+    })
+
+    function handleBoxClick() {
+        const newColor = getRandomColor();
+        setColor(newColor);
+        localStorage.setItem('box_color', newColor)
+    }
+
+    return (
+        <div className="color-box"
+            style={{ backgroundColor: color }}
+            onClick={handleBoxClick}
+        >
+            COLOR BOX
+        </div>
+    );
+}
+
+export default ColorBox;
