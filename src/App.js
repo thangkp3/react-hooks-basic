ddc76bf95d6c6c0a2a81237e2d482bbf3a7659a9
thangkp3 <thangkp3@gmail.com>
2020-08-03 18:13:40 +0700

    remove todolist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import ColorBox from './components/ColorBox';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: 'Hello' },
+    { id: 2, title: 'Hi' },
+    { id: 3, title: 'Welcome' },
+  ]);
+
+  function handleTodoClick(todo) {
+    console.log(todo);
+    const index = todoList.findIndex(x => x.id === todo.id);
+    if (index < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="app">
-      <ColorBox />
+
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
